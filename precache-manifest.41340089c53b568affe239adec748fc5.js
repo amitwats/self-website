453c5491689e928cf8c0ce40fac229272637fc0e
amitwats <amitwats@gmail.com>
2019-12-20 06:42:18 +1100

    Updates

---
+++ precache-manifest.41340089c53b568affe239adec748fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daab86ff907a357e396f9b12e16016f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32f37f403336af30c1ef",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3c67739c5699a48e467d",
+    "url": "/static/js/2.cf9b5d9b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cf9b5d9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32f37f403336af30c1ef",
+    "url": "/static/js/main.b086cf85.chunk.js"
+  },
+  {
+    "revision": "cdb471905a4c85b4f014",
+    "url": "/static/js/runtime-main.483601b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
